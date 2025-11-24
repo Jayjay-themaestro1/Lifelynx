bea48346250964be32d72dbf21173627c147b1d5
Jayjay-themaestro1 <jayjaythemaestro@gmail.com>
2025-11-24 15:42:42 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+
+function register() {
+    window.location.href = "registration.html";
+}
+
+function login() {
+    window.location.href = "log_in.html"
+}
+
+function loggedIn() {
+    window.location.href = "services.html"
+}
+
+// const navMenu = document.getElementById("menu");
+
+function toggleMenu() {
+    const menu = document.getElementById("menu");
+    menu.style.display = (menu.style.display === "block") ? "none" : "block";
+}
+
+function joinCall() {
+}
+
